444688f51f462b0973edb301905f1df5a66f9555
suddiyo <suddii01@sju.ac.kr>
2023-09-03 16:14:42 +0900

    Update(FE): Step2 css 보완

--- src/main/frontend/src/components/ImageSlider.js
+++ src/main/frontend/src/components/ImageSlider.js
@@ -10,7 +10,32 @@ const ImageSlider = ({ setClickedItem }) => {
     const [currentSlide, setCurrentSlide] = useState(0);
     const [clickedItem, setLocalClickedItem] = useState(null);
 
-
+    const commentData = [
+        {
+            index: 1,
+            comment: "#청량한 #파랑이돋보이는 #신비로운",
+        },
+        {
+            index: 2,
+            comment: "#우아한 #모던한 #주황포인트",
+        },
+        {
+            index: 3,
+            comment: "#포근한 #자연주의 #하양초록",
+        },
+        {
+            index: 4,
+            comment: "#로맨틱 #화려 #핑크빛 #세련된",
+        },
+        {
+            index: 5,
+            comment: "#분홍빛의 #로맨틱 #세련된",
+        },
+        {
+            index: 6,
+            comment: "#모던한 #시크 #깔끔한 #유럽풍",
+        },
+    ];
 
     const sliderTrackRef = useRef(null);
 
@@ -72,7 +97,16 @@ const ImageSlider = ({ setClickedItem }) => {
                                             opacity: clickedItem === null ? 1 : clickedItem === itemIndex ? 1 : 0.4,
                                         }}
                                     />
-
+                                    {clickedItem === itemIndex && (
+                                        <div>
+                                            <div className="image-blur-overlay"></div>
+                                            <div className="step2-design-comment-container">
+                                                <div className={`step2-design-comment ${clickedItem === itemIndex ? 'visible' : ''} `}>
+                                                    {commentData.find((data) => data.index === itemIndex)?.comment}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
                                 </div>
                             </div>
                         ))}
